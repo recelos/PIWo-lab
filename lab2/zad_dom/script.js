f9784a4ec76c8237724b45c2f0c3e493b1633d84
recelos <kubagr2000@gmail.com>
2023-04-25 21:05:13 +0200

    Working website

---
+++ lab2/zad_dom/script.js
@@ -0,0 +1,140 @@
+"use strict";
+
+const notUrgentList = [];
+const urgentList = [];
+const criticalList = [];
+
+let lastDeletedTask = null;
+
+const createTaskElement = (task, list) => {
+    const div = document.createElement("div");
+    div.classList.add("task");
+  
+    const p = document.createElement("p");
+    p.textContent = task.name;
+  
+    if (task.isFinished) {
+        const s = document.createElement("s");
+        s.textContent = task.name;
+        p.innerHTML = "";
+        p.appendChild(s);
+  
+        const dateP = document.createElement("p");
+        dateP.textContent = "Done! on " + task.finishedDate;
+  
+        div.appendChild(dateP);
+    }
+  
+    div.appendChild(p);
+  
+    const finishButton = document.createElement("button");
+    finishButton.innerHTML = "✅";
+
+    // check if handler has already been assigned
+    finishButton.onclick = finishButton.onclick ? finishButton.onclick : () => handleFinishButtonClick(task);
+    
+    div.appendChild(finishButton);
+  
+    const deleteButton = document.createElement("button");
+    deleteButton.innerHTML = "🗑️";
+
+    // check if handler has already been assigned
+    deleteButton.onclick = deleteButton.onclick ? deleteButton.onclick : () => handleDeleteTask(task, list);
+
+    const btnDiv = document.createElement("div");
+    btnDiv.append(finishButton, deleteButton)
+
+    div.appendChild(btnDiv);
+
+    return div;
+}
+  
+const handleFinishButtonClick = (task) => {
+    task.isFinished = !task.isFinished;
+    task.finishedDate = task.isFinished
+        ? new Date(Date.now()).toLocaleDateString()
+        : null;
+    renderTasks();
+}
+
+const handleAddTask = (input ,list) => {
+    const taskName = input.value;
+
+    if(!taskName){
+        alert("Task cannot be empty!");
+        return;
+    }
+
+    const newTask = {
+        name: taskName,
+        isFinished: false,
+        finishedDate: null
+    }
+
+    list.push(newTask);
+    renderTasks();
+}
+
+const addHandlers = () => {
+    const notUrgentInput = document.getElementById("not-urgent-input");
+    const notUrgentAddButton = document.getElementById("not-urgent-add-button");
+    notUrgentAddButton.onclick = () => handleAddTask(notUrgentInput, notUrgentList);
+    
+    const urgentInput = document.getElementById("urgent-input");
+    const urgentAddButton = document.getElementById("urgent-add-button");
+    urgentAddButton.onclick = () => handleAddTask(urgentInput, urgentList);
+    
+    const criticalInput = document.getElementById("critical-input");
+    const criticalAddButton = document.getElementById("critical-add-button");
+    criticalAddButton.onclick = () => handleAddTask(criticalInput, criticalList);
+}
+
+const handleDeleteTask = (task, list) => {
+    const index = list.indexOf(task);
+    if(index === -1)
+        return;
+    
+    list.splice(index, 1);
+    lastDeletedTask = 
+    {
+        deletedTask: task,
+        fromList: list
+    };
+
+    enableRestoreButton();
+    renderTasks();
+}
+
+const renderTasks = () => {
+    const notUrgentTaskContainer = document.getElementById("not-urgent-tasks");
+    const notUrgentTasks = notUrgentList.map(task => createTaskElement(task, notUrgentList));
+    notUrgentTaskContainer.replaceChildren(...notUrgentTasks);
+
+    const urgentTaskContainer = document.getElementById("urgent-tasks");
+    const urgentTasks = urgentList.map(task => createTaskElement(task, urgentList));
+    urgentTaskContainer.replaceChildren(...urgentTasks);
+
+    const criticalTaskContainer = document.getElementById("critical-tasks");
+    const criticalTasks = criticalList.map(task => createTaskElement(task, criticalList));
+    criticalTaskContainer.replaceChildren(...criticalTasks);
+}
+
+const enableRestoreButton = () => {
+    const restoreButton = $("#restore"); 
+
+    if(lastDeletedTask === null){
+        restoreButton.prop("disabled", true);
+        return;
+    }
+    restoreButton.prop("disabled", false);
+}
+
+const handleRestoreButton = () => {
+    if(lastDeletedTask === null)
+        return;
+    
+    lastDeletedTask.fromList.push(lastDeletedTask.deletedTask);
+    lastDeletedTask = null;
+    renderTasks();
+    enableRestoreButton();
+}
