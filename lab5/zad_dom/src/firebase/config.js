b9308705af25b7424fb16b92837420621bf1841d
recelos <kubagr2000@gmail.com>
2023-06-05 21:27:24 +0200

    Delete config

--- lab5/zad_dom/src/firebase/config.js
+++
@@ -1,8 +0,0 @@
-export const config = {
-  apiKey: "AIzaSyAoAFy2grPkUOLeTHGWYvgszMC4Zw2uicY",
-  authDomain: "evilla-5198b.firebaseapp.com",
-  projectId: "evilla-5198b",
-  storageBucket: "evilla-5198b.appspot.com",
-  messagingSenderId: "1080150207319",
-  appId: "1:1080150207319:web:0f10f7aaaad6b765d9ac12"
-};
